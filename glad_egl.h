24b46d56cabf4205863555614cac6ae6b7eca95c
Andrew Johnson <andrew@nira.app>
2025-07-28 18:19:34 -0700

    Point to local EGL and KHR dirs.

--- glad_egl.h
+++ glad_egl.h
@@ -217,8 +217,8 @@ typedef void* (* GLADloadproc)(const char *name);
 GLAPI int gladLoadEGL(void);
 GLAPI int gladLoadEGLLoader(GLADloadproc);
 
-#include <KHR/khrplatform.h>
-#include <EGL/eglplatform.h>
+#include "KHR/khrplatform.h"
+#include "EGL/eglplatform.h"
 struct AHardwareBuffer;
 struct wl_buffer;
 struct wl_display;
